689b160877a01b491d109a1f878afd314c47fb98
ivyksy <ivyksy0215@naver.com>
2024-05-13 19:05:25 +0900

    사용자 입력

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import User from './assets/User';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
@@ -28,6 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <User></User>
     </>
   )
 }
